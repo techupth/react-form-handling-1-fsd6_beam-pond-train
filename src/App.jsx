093980571815367af898969d820dcddefb6cfefc
Jakkaphob <jkpbeam@gmail.com>
2024-06-05 13:12:04 +0700

    feat: add react state assignment 1 as feature/beam

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetMessage, setGreetMessage] = useState("Greeting Message");
+  const [input, setInput] = useState("");
+
+  const update = () => {
+    setGreetMessage(input);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetMessage}</div>
       <div className="input-container">
-        <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <label htmlFor="greeting-message">
+          <input
+            id="greeting-message"
+            type="text"
+            value={input}
+            onChange={ (event) =>{setInput(event.target.value)}}
+          />
+        </label>
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={update}>Update text</button>
       </div>
     </div>
   );
