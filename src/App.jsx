78e37bcabb4f244505c59bb545447451ffce458b
Pond <pondwachi.th@gmail.com>
2024-06-05 13:29:51 +0700

    faet: react form of pond

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import "./App.css";
+import { useState } from "react";
+
+
 
 function App() {
+  const [text, setText] = useState('greetingTextChange')
+  const [inputText, setInputText] = useState('')
+
+  const handInputCahnge = (event) => {
+    setInputText(event.target.value)
+  }
+
+  const updateText = () => {
+    setText(inputText)
+  }
+  
+
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{text}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input id="greeting-message" type="text" value={inputText} onChange={handInputCahnge}/>
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={updateText}>Update text</button>
       </div>
     </div>
   );
